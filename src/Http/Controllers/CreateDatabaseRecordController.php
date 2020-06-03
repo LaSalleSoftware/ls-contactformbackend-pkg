92826b58a886415e14dbcb663b466d127f287e10
Bob Bloom <bob.bloom@lasallesoftware.ca>
2020-06-03 13:17:39 -0400

    Create a UUID when inserting contact form data into the db. #14

--- src/Http/Controllers/CreateDatabaseRecordController.php
+++ src/Http/Controllers/CreateDatabaseRecordController.php
@@ -25,6 +25,7 @@ namespace Lasallesoftware\Contactformbackend\Http\Controllers;
 // LaSalle Software
 use Lasallesoftware\Contactformbackend\Models\Contact_form;
 use Lasallesoftware\Librarybackend\Common\Http\Controllers\CommonController;
+use Lasallesoftware\Librarybackend\UniversallyUniqueIDentifiers\UuidGenerator;
 
 // Laravel
 use Illuminate\Support\Facades\Request;
@@ -39,8 +40,30 @@ use Carbon\Carbon;
  */
 class CreateDatabaseRecordController extends CommonController
 {
+    /**
+     * Uuidgenerator class
+     *
+     * @var Lasallesoftware\Librarybackend\UniversallyUniqueIDentifiers\UuidGenerator
+     */
+    protected $uuidgenerator;
+    
+
+    /**
+     * Create a new CreateDatabaseRecordController instance
+     *
+     * @param  Lasallesoftware\Librarybackend\UniversallyUniqueIDentifiers\UuidGenerator  $uuidgenerator
+     * @return void
+     */
+    public function __construct(UuidGenerator $uuidgenerator)
+    {
+        $this->uuidgenerator = $uuidgenerator;
+    }
+
     public function HandleCreateDatabaseRecord(Contact_form $contact_form)
-    { 
+    {
+        $comment = 'Lasallesoftware\Contactformbackend\Http\Controllers\HandleCreateDatabaseRecordInsert - insert the contact form submission data into the database.';
+        $uuid = $this->uuidgenerator->createUuid(11, $comment, 1);
+
         $postData['installed_domain_id'] = $_POST["installed_domain_id"];
 
         // The method that gets the personbydomain_id, Lasallesoftware\Contactformfrontend\Http\Controllers::getPersonbydomainId(),
@@ -52,7 +75,7 @@ class CreateDatabaseRecordController extends CommonController
         $postData['surname']             = $_POST["surname"];
         $postData['email']               = $_POST["email"];
         $postData['message']             = $_POST["comment"];
-        $postData['uuid']                = $_POST["uuid"];
+        $postData['uuid']                = $uuid;
         $postData['created_at']          = Carbon::now(null);
 
         $result = $contact_form->createNewContactformRecord($postData);
