2b180d827b928fa88dda25f022eec8b05c58372b
Bob Bloom <bob.bloom@lasallesoftware.ca>
2020-07-01 11:31:15 -0400

    Add 2 database calls extracted from the contactform front-end package. #16

--- src/Http/Controllers/CreateDatabaseRecordController.php
+++ src/Http/Controllers/CreateDatabaseRecordController.php
@@ -27,7 +27,8 @@ use Lasallesoftware\Contactformbackend\Models\Contact_form;
 use Lasallesoftware\Librarybackend\Common\Http\Controllers\CommonController;
 use Lasallesoftware\Librarybackend\UniversallyUniqueIDentifiers\UuidGenerator;
 
-// Laravel
+// Laravel Facades
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Request;
 
 // Third party class
@@ -65,13 +66,9 @@ class CreateDatabaseRecordController extends CommonController
         $comment = 'Lasallesoftware\Contactformbackend\Http\Controllers\HandleCreateDatabaseRecordInsert - insert the contact form submission data into the database.';
         $uuid = $this->uuidgenerator->createUuid(11, $comment, 1);
 
-        $postData['installed_domain_id'] = $_POST["installed_domain_id"];
+        $postData['installed_domain_id'] = $this->getInstalled_domain_id($_POST["lasalle_app_domain_name"]);
 
-        // The method that gets the personbydomain_id, Lasallesoftware\Contactformfrontend\Http\Controllers::getPersonbydomainId(),
-        // returns 0 when no personbydomain_id is found. The reason is that an actual value must be transmitted via the post request. 
-        // A null value, or a blank value, does not transmit. So I use the value "0" to denote a null value.
-
-        $postData['personbydomain_id']   = ($_POST["personbydomain_id"] == 0) ? null : $_POST["personbydomain_id"];
+        $postData['personbydomain_id']   = $this->getPersonbydomainId($_POST["email"]);
         $postData['first_name']          = $_POST["first_name"];
         $postData['surname']             = $_POST["surname"];
         $postData['email']               = $_POST["email"];
@@ -84,5 +81,37 @@ class CreateDatabaseRecordController extends CommonController
         return response()->json([
             'result' => $result,
         ], 200);
+    }
+
+
+    /**
+     * Get the personbydomains' ID given an email address. 
+     * 
+     * @param  string  $email  
+     * @return int 
+     */
+    public function getPersonbydomainId($email)
+    {
+        $result = DB::table('personbydomains')
+            ->where('email', $email)
+            ->get()
+        ;
+
+        return ($result->isEmpty()) ? null : $result[0]->id;
+    }
+    
+    /**
+     * Get the installed_domain_id from the installed_domains db table for this front-end app.  
+     * 
+     * @param  string  $lasalle_app_domain_name    The env('LASALLE_APP_DOMAIN_NAME) of the front-end app sending the request
+     * @return mixed 
+     */
+    public function getInstalled_domain_id($lasalle_app_domain_name)
+    {
+        return DB::table('installed_domains')
+            ->where('title', $lasalle_app_domain_name)
+            ->pluck('id')
+            ->first()
+        ;
     }
 }
